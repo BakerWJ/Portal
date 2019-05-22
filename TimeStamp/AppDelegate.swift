49fa6ceda0fcdf4c22498387a1f2a824d1df45ce
Baker Jackson <jacba@utschools.ca>
2019-05-22 10:30:58 -0400

    fixed error with connecting to server

--- TimeStamp/AppDelegate.swift
+++ TimeStamp/AppDelegate.swift
@@ -56,7 +56,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate
         FirebaseApp.configure();
         //After the app launches, it will check if the current schedule stored locally is up to date
         UserDataSettings.delegate = self;
-        UserDataSettings.updateAll()
         //if notifications are not allowed, it is asked for
         notificationCenter.getNotificationSettings { (settings) in
             if settings.authorizationStatus != .authorized {
@@ -68,6 +67,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
                 }
             }
         }
+        UserDataSettings.updateAll()
         return true
     }
     
