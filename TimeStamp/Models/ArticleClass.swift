1624f18ce128b8f3e60ac3d4874feae1d16afcfa
Baker Jackson <jacba@utschools.ca>
2019-08-24 19:17:57 -0400

    done with news :) now to data!

--- TimeStamp/Models/ArticleClass.swift
+++ TimeStamp/Models/ArticleClass.swift
@@ -17,15 +17,17 @@ class Article {
     var title: String
     var genre: String
     var likes: Int
+    var hash: String
     
     
     //Initializer for article class
-    init(text: String, author: String, img: UIImage, title: String, genre:String, likes:Int) {
+    init(text: String, author: String, img: UIImage, title: String, genre:String, likes:Int, hash:String) {
         self.text = text
         self.author = author
         self.img = img
         self.title = title
         self.genre = genre
         self.likes = likes
+        self.hash = hash
     }
 }
