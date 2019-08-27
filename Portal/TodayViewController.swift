82a29f7c0e97ad0be142b9c94be0cb83c3af60ce
Jacky He <heja@utschools.ca>
2019-08-27 19:58:34 -0400

    bug fix

--- Portal/TodayViewController.swift
+++ Portal/TodayViewController.swift
@@ -32,16 +32,15 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         nextPeriodView.heightAnchor.constraint(equalToConstant: 54).isActive = true;
         nextPeriodView.layoutIfNeeded()
         nextPeriodView.layer.cornerRadius = nextPeriodView.frame.height/2;
-        nextPeriodView.refresh()
     }
-        
+
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-        nextPeriodView.refresh()
+        
         completionHandler(NCUpdateResult.newData)
     }
     
